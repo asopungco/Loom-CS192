bf5d7748876bfc639e9a2b7d318e1762fc487337
asopungco <91015701+asopungco@users.noreply.github.com>
2022-03-04 11:09:16 +0800

    Added main.py and Loom.kv

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+class ActualSample(BoxLayout):
+	pass
+
+class GridLayoutExample(GridLayout):
+	pass
+
+class AnchorLayoutExample(AnchorLayout):
+	pass
+
+class BoxLayoutExample(BoxLayout):
+	pass
+
+class MainWidget(Widget):
+	pass
+
+class LoomApp(App):
+	pass
+
+LoomApp().run()
